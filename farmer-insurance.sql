bc1eeeab6fedb8c1cd63a93c4aa00b001bcee87c
Shikha Singh <ss8086909@gmail.com>
2025-04-07 16:11:13 +0530

    Make all the required changes

--- farmer-insurance.sql
+++ farmer-insurance.sql
@@ -3,9 +3,9 @@ use assignment;
 select * from data
 /* SELECTQueries [5 marks]
 1. Retrieve the names of all states (srcStateName).
-2. RetrieveTotalFarmersCoveredandSumInsuredfor  each  state,  ordered  descending  byTotalFarmersCovered.
 */
  Select srcStateName from data;
+ 
  /*2. RetrieveTotalFarmersCoveredandSumInsuredfor  each  state,  ordered  descending  byTotalFarmersCovered.*/
  
 SELECT srcStateName, 
@@ -53,11 +53,7 @@ select  avg(InsuredLandArea) as insuredlandyear , srcYear from data
  
  /*IV.  Sorting Data (ORDER BY) [10 marks]
  10. Retrieve the top 5 districts with the highest total population in 2020.
- 11. Retrieve srcStateName,srcDistrictName, andSumInsured for the 10 districts
- with lowestnon-zero farmers’ premium amount, ordered by insured sum and premium amount.
- 12. Retrieve top 3 states for each year with highest insured farmers to total population ratio, orderedby
- the ratio*/
- 
+ */
  select srcDistrictname, sum(TotalPopulation) as TotalPopulation
  from data
  where YearCode= 2020
@@ -83,24 +79,19 @@ ROUND(SUM(SumInsured) / SUM(TotalPopulation), 3) AS total_population_ratio
  order by total_population_ratio desc limit 3;
  
  /*String Functions [6 marks]13. 
- Retrieve the first 3 characters of the srcStateName to create StateShortName.
- 14. Retrieve srcDistrictName where the district name starts with"B".
- 15. Retrieve srcStateNameandsrcDistrictNamewhere district name ends with"pur"*/
+ Retrieve the first 3 characters of the srcStateName to create StateShortName.*/
  
- Select substr(srcStateName,1, 3) as StateShortName  from data; 
+ Select substr(srcStateName,1, 3) as StateShortName  from data;
+ /*14. Retrieve srcDistrictName where the district name starts with"B".*/
+ 
  select srcDistrictName from data where srcDistrictName  like "B%";
+ /* 15. Retrieve srcStateNameandsrcDistrictNamewhere district name ends with"pur"*/
  select srcStateName, srcDistrictName from data  where    srcDistrictName like "%pur";
  
  /*joins [14 marks]16. 
  INNER JOINsrcStateNameandsrcDistrictNameto retrieve the aggregated farmers’ premiumamount for districts 
  where the Insurance units for an individual year are greater than 10.
- 17. RetrievesrcStateName,srcDistrictName,Year,TotalPopulationfor each district and thethe 
- highest recorded farmers premium amount for that district over all available years. 
- Returnonly those districts where the highest amount exceeds 20 crores.18. Perform a LEFT JOIN 
- to combine the total population statistics with the farmers’ data for eachdistrict and state. 
- Return the total premium amount and the average population count for eachdistrict aggregated over the
- years, where the total premium amount is greater than 100 crores.Sort the results by total farmers’ 
- premium amount, highest first.*/
+ */
  
  select d1.srcStateName, d1.srcDistrictName, d1.srcYear, SUM(d2.FarmersPremiumAmount) AS TotalFarmersPremium
  from data d1
@@ -111,7 +102,9 @@ d1.srcYear=d2.srcYear
 where d1.Insuranceunits>10
 group by d1.srcStateName, d1.srcDistrictName, d1.srcYear;
  
-
+/* 17. RetrievesrcStateName,srcDistrictName,Year,TotalPopulationfor each district and thethe 
+ highest recorded farmers premium amount for that district over all available years. 
+ Returnonly those districts where the highest amount exceeds 20 crores.*/
 -- Subquery to get max FarmersPremiumAmount per district
 SELECT 
     d.srcStateName,
@@ -133,6 +126,12 @@ ON d.srcStateName = max_table.srcStateName
    AND d.srcDistrictName = max_table.srcDistrictName
    AND d.FarmersPremiumAmount = max_table.MaxPremium;
    
+   /*18. Perform a LEFT JOIN 
+ to combine the total population statistics with the farmers’ data for eachdistrict and state. 
+ Return the total premium amount and the average population count for eachdistrict aggregated over the
+ years, where the total premium amount is greater than 100 crores.Sort the results by total farmers’ 
+ premium amount, highest first.*/
+   
    SELECT 
     d.srcStateName,
     d.srcDistrictName,
@@ -149,10 +148,10 @@ ORDER BY TotalFarmersPremium DESC;
 /*19. Districts where total farmers covered is greater than average total farmers 
 covered across all records:*/
 select srcDistrictName from data where TotalFarmersCovered >
-(select avg(TotalFarmersCovered) from data)
+(select avg(TotalFarmersCovered) from data);
 
 /*Find srcStateName where insured sum is higher than 
-insured sum of the district with the highestfarmers’ premium amount */
+insured sum of the district with the highestfarmers’ premium amount*/
 SELECT srcStateName
 FROM data
 WHERE SumInsured > (
@@ -163,6 +162,7 @@ WHERE SumInsured > (
         FROM data
     )
 );
+/*FindsrcDistrictNamewhere farmers’ premium amount is higher than average farmers’ premiumamount of the state with highest total population.*/
 
 SELECT srcDistrictName, srcStateName, FarmersPremiumAmount
 FROM data
@@ -178,11 +178,9 @@ WHERE FarmersPremiumAmount > (
     )
 );
 
-/*dvanced SQL Functions (Window Functions) [10 marks]22. UseROW_NUMBER()to rank records ordered by the
+/*advanced SQL Functions (Window Functions) [10 marks]22. UseROW_NUMBER()to rank records ordered by the
  total farmers covered.
- 23. UseRANK()to rank districts based on insured sum partitioned bysrcStateName.
- 24. UseSUM()window function to calculate cumulative farmers’
- premium amount for each districtpartitioned bysrcStateName*/
+ */
  
  SELECT 
     srcStateName,
@@ -190,14 +188,15 @@ WHERE FarmersPremiumAmount > (
     TotalFarmersCovered,
     ROW_NUMBER() OVER (ORDER BY TotalFarmersCovered DESC) AS row_rank
 FROM data;
-
+/*23. UseRANK()to rank districts based on insured sum partitioned bysrcStateName.*/
 SELECT 
     srcStateName,
     srcDistrictName,
     SumInsured,
     RANK() OVER (PARTITION BY srcStateName ORDER BY SumInsured DESC) AS state_rank
 FROM data;
-
+/*24. UseSUM()window function to calculate cumulative farmers’
+ premium amount for each districtpartitioned bysrcStateName*/
 SELECT 
     srcStateName,
     srcDistrictName,
@@ -206,7 +205,7 @@ SELECT
     SUM(FarmersPremiumAmount) OVER (PARTITION BY srcStateName, srcDistrictName ORDER BY srcYear) AS cumulative_premium
 FROM data;
 
-/*ata Integrity (Constraints, Foreign Keys) [4 marks]25. Create a tabledistrictswithDistrictCodeas the primary key and columns forDistrictNameandStateCode. Create another tablestateswithStateCodeas primary key and column forStateName.2
+/*Data Integrity (Constraints, Foreign Keys) [4 marks]25. Create a tabledistrictswithDistrictCodeas the primary key and columns forDistrictNameandStateCode. Create another tablestateswithStateCodeas primary key and column forStateName.2
 26. Add a foreign key constraint to the districts table referencingStateCodefrom the states table.*/
 
 CREATE TABLE states (
@@ -222,23 +221,20 @@ CREATE TABLE districts (
 );
 
 /*UPDATEandDELETE[6 marks]
-27. Update FarmersPremiumAmountto 500.0 INR whererowID = 1.28.
- Update year to 2021 for records wheresrcStateName = "HIMACHAL PRADESH".
- 29. Delete records whereTotalFarmersCovered < 10,000andYear = 2020
- */
+27. Update FarmersPremiumAmountto 500.0 INR whererowID = 1.28.*/
  update data 
  set FarmersPremiumAmount = 500.0
  where rowID = 1.28;
  
- set SQL_SAFE_UPDATES= 0
- 
+ set SQL_SAFE_UPDATES= 0;
+ /*Update year to 2021 for records wheresrcStateName = "HIMACHAL PRADESH".*/
  update data
  set srcYear=2021
- where srcStateName = "HIMACHAL PRADESH"
- 
+ where srcStateName = "HIMACHAL PRADESH";
+ /* 29. Delete records whereTotalFarmersCovered < 10,000andYear = 2020*/
  delete from data
- where TotalFarmersCovered < 10000 and srcYear = 2020
- set SQL_SAFE_UPDATES= 1
+ where TotalFarmersCovered < 10000 and srcYear = 2020;
+ set SQL_SAFE_UPDATES= 1;
 
  
  
